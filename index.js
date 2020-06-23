6021721acebbf47caddb7fb54f97461fb609d4d8
DaleKirkman <64217274+DaleKirkman@users.noreply.github.com>
2020-06-23 14:36:01 +0100

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-var imgArr = ["pod1.png","pod2.png","pod3.png"];
-
-function displayPodcast(){
-    var podcasts = Math.floor(Math.random() * (imgArr.length));
-    document.podcasts.src="images-podcasts/" + imgArr[podcasts];
-}
